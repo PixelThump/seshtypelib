9f82c8ca060e0a713c659e1e18531807feb42ecc
roboter5123 <73991535+roboter5123@users.noreply.github.com>
2023-08-31 12:45:42 +0200

    Updated Config

--- src/main/java/com/pixelthump/seshtypelib/config/CorsConfig.java
+++ src/main/java/com/pixelthump/seshtypelib/config/CorsConfig.java
@@ -3,9 +3,11 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 
 @Configuration
 @ConfigurationProperties(prefix = "pixelthump.seshtype.cors")
+@ConditionalOnMissingBean
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
--- src/main/java/com/pixelthump/seshtypelib/config/SpringBootSeshtypelibAutoConfiguration.java
+++ src/main/java/com/pixelthump/seshtypelib/config/SpringBootSeshtypelibAutoConfiguration.java
@@ -11,6 +11,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 
 @Configuration
 @ConfigurationProperties(prefix = "pixelthump.seshtype")
