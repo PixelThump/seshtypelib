3c94395ed3539d9475256d3b115b1de641c29d2b
roboter5123 <73991535+roboter5123@users.noreply.github.com>
2023-08-31 12:42:12 +0200

    Updated configuration

--- src/main/java/com/pixelthump/seshtypelib/config/SpringBootSeshtypelibAutoConfiguration.java
+++ src/main/java/com/pixelthump/seshtypelib/config/SpringBootSeshtypelibAutoConfiguration.java
@@ -13,40 +13,47 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@EnableAutoConfiguration
 @ConfigurationProperties(prefix = "pixelthump.seshtype")
 public class SpringBootSeshtypelibAutoConfiguration {
 
     @Bean
+    @ConditionalOnMissingBean
     public RestTemplate restTemplate(){
 
         return new RestTemplate();
     }
 
     @Bean
+    @ConditionalOnMissingBean
     public ModelMapper modelMapper(){
 
         return new ModelMapper();
     }
 
     @Bean
+    @ConditionalOnMissingBean
     public BroadcastService broadcastService(RestTemplate restTemplate, StateService stateService){
 
         return new BroadcastServiceRestImpl(restTemplate,stateService);
     }
 
     @Bean
+    @ConditionalOnMissingBean
     public SeshService seshService(StateFactory stateFactory, StateService stateService, CommandRespository commandRespository, PlayerService playerService, GameLogicService gameLogicService, BroadcastService broadcastService){
 
         return new SeshServiceImpl(stateFactory,stateService,commandRespository,playerService, gameLogicService, broadcastService);
     }
 
     @Bean
+    @ConditionalOnMissingBean
     public PingResource pingResource(){
 
         return new PingResource();
     }
 
     @Bean
+    @ConditionalOnMissingBean
     public SeshResource seshResource(SeshService seshService, ModelMapper modelMapper){
 
         return new SeshResource(seshService, modelMapper);
@@ -56,6 +63,7 @@ public class SpringBootSeshtypelibAutoConfiguration {
     private String url;
 
     @Bean
+    @ConditionalOnMissingBean
     public OpenAPI openAPI() {
         return new OpenAPI()
                 .addServersItem(new Server().url(url));
